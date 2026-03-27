227fbc1cb16742b0ed1c2f9d88459e9a7f298ace
Manish Kumar <manishbksc26gmail.com>
2026-03-27 21:03:31 +0530

    added new Demo2 java file

--- Demo2.java
+++ Demo2.java
@@ -3,7 +3,7 @@ public class Demo2 {
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Tic Tac Toe Game!");
-        System.out.println("Player 1: X");
+        System.out.println("Player 1: y");
         System.out.println("Player 2: O");
     }
 }
