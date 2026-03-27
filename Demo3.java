02ac9575c7c0d9cfa1d6c8eea3df167f4ee80736
Manish Kumar <manishbksc26gmail.com>
2026-03-27 21:07:05 +0530

    Adding Demo3 in Kanhaiya branch

---
+++ Demo3.java
@@ -0,0 +1,9 @@
+public class Demo3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Tic Tac Toe Game!");
+        System.out.println("Player 1: A");
+        System.out.println("Player 2: B");
+    }
+}
